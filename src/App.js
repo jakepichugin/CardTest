5d237254b3683ff2638bdf21f8ca39436d704336
Jake Pichugin <jakepichugin@gmail.com>
2022-04-16 18:56:28 -0400

    class3:download card not finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef, createRef } from 'react'
 import {styles} from './styles'
+import { exportComponentAsPNG } from 'react-component-export-image';
+
 const githuburl = 'https://api.github.com/users'
-
-
 
 const App = () => {
 
+  const imgref = [];
   const [users, set_users] = useState([])
 
   useEffect(() => {
@@ -15,17 +16,20 @@ const App = () => {
     }).catch((err) => { console.log(err)})
   }, [])
 
+   const setRef = (ref) => {
+    imgref.push(ref);
+  };
 
-
+  const getRef = (ref) => imgref.find(ref);
 
   return (
-    <div>
+    <div style={styles.container}>
       {users.map((u)=>{
         return (
-          <div style={styles.card_container}> 
+          <div ref={setRef} key={u.id} style={styles.card_container} onClick={(e) => exportComponentAsPNG(imgref[e])}> 
             <img src={u.avatar_url} style={styles.card_art}/>
-            <div>{u.login} </div>
-            <div>{u.html_url} </div>
+            <div style={styles.card_name} > {u.login} </div>
+            <div style={styles.card_text}>{u.html_url} </div>
 
             </div>
         )
--- src/styles.js
+++ src/styles.js
@@ -1,7 +1,14 @@
 export const styles = {
+    container:{
+        width:'100%',
+        height:'100%',
+        display:'grid',
+        gridTemplateColumns:'auto auto auto',
+    },
+
     card_container:{
-        width:252,
-        height:352,
+        width:'252px',
+        height:'352px',
         border:'10px solid #f5d742',
         borderRadius:'8px',
         backgroundColor:'#a27e5d',
@@ -11,11 +18,29 @@ export const styles = {
 
     },
     card_art:{
-        width:'100%',
-        height:'45',
+        width:'99%',
+        height:'45%',
         border:'2px solid #d8b827',
         borderRadius:'8px',
     },
-    card_name:{},
-    card_text:{},
+    card_name:{
+        fontFamily:'monospace',
+        fontSize:'2em',
+        textAlign:'center',
+        border:'1px solid #a27e5d',
+        backgroundColor:'#997453',
+        borderRadius:'50px'
+
+    },
+    card_text:{
+        fontFamily:'monospace',
+        textAlign:'center',
+        marginTop:'10px',
+        width:'97%',
+        height:'33%',
+        border:'2px solid #d8b827',
+        borderRadius:'8px',
+        backgroundColor:'#997453',
+        padding:'1%'
+    },
 }
